dd1836e12a1975926c256d718c30ba7fa6f6c483
Alex Trukhny <atrukhny@bp-3.com>
2026-02-25 10:20:49 +0200

    Fix logging, columns in team report

--- src/logger.ts
+++ src/logger.ts
@@ -2,11 +2,17 @@ import pino from "pino";
 import { AppConfig } from "./config";
 
 export function createLogger(config: AppConfig) {
+
   return pino({
     level: config.logging.level,
+    base: null,
     transport: {
       target: "pino-pretty",
-      options: { colorize: true }
-    }
+      options: {
+        colorize: true,
+        ignore: "pid,hostname",
+        translateTime: "HH:MM:ss",
+      },
+    },
   });
 }
